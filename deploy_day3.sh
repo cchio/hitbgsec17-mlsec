dc6b5f9be75b92610be093210e3272c475a45951
Clarence Chio <chiochio@gmail.com>
2017-08-23 07:30:45 +0800

    Added day3 materials

---
+++ deploy_day3.sh
@@ -0,0 +1,8 @@
+mkdir ~/Desktop/malware/classifier
+chown -R ml:ml ~/Desktop/malware/classifier
+pip install requests
+pip install https://github.com/guelfoweb/peframe/archive/master.zip
+pip install simplejson
+cd ~/Desktop/cleverhans
+git pull
+pip install -e git+http://github.com/tensorflow/cleverhans.git#egg=cleverhans
